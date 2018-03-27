d44656ed0b758a6e7a1a7ef4553c4010bf3ade3b
willhickey <csu_hickey@Yahoo.com>
2018-03-26 19:59:28 -0600

    reverted to static read length

--- SplitAndReassemble/helperFunctions.py
+++ SplitAndReassemble/helperFunctions.py
@@ -7,9 +7,11 @@ def getInsertLength():
 #	the ReadLength table contains a distribution of read lengths generated from 
 #	real MiSeq reads of NTM. This function generates a pseudo random number and then
 #	usues it to lookup the associated read length from the database
+#	The DB lookups slow things down too much, reverting to a static 250 for now
 def getReadLength(cursor, nominalReadLength, direction):
-	myRand = random.random()
-	sql = 'SELECT ReadLength FROM ReadLength WHERE LowerBound <= ? AND ? < UpperBound AND NominalReadLength = ? AND Direction = ?'
-	#could use some error handling here...
-	cursor.execute(sql, (myRand, myRand, nominalReadLength, direction))
-	return cursor.fetchone()[0]
+	return 250
+	#myRand = random.random()
+	#sql = 'SELECT ReadLength FROM ReadLength WHERE LowerBound <= ? AND ? < UpperBound AND NominalReadLength = ? AND Direction = ?'
+	##could use some error handling here...
+	#cursor.execute(sql, (myRand, myRand, nominalReadLength, direction))
+	#return cursor.fetchone()[0]
